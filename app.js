c805fde385c191d8980909af4e73aba98984ac5f
qirall79 <belfatmi79@gmail.com>
2023-01-16 20:41:02 +0100

    Add changes required for deployment

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
 // Setting up MongoDB
 const mongoose = require("mongoose");
-const mongoDB =
-  "mongodb+srv://walid:walid123@cluster0.hqkhs9t.mongodb.net/local_library?retryWrites=true&w=majority";
 
+const dev_db_url =
+  "mongodb+srv://walid:walid123@cluster0.hqkhs9t.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+// helmet setup
+app.use(helmet());
+
+// compression setup
+app.use(compression());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
